421eaac9d7350fb94ab92a3c58a296ddbae177ad
Anuradha Hariharan <anuradhahariharan@Anuradhas-MacBook-Air.local>
2024-11-03 01:14:55 +0530

    resume update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -57,7 +57,7 @@ const NavBar = () => {
               </Nav>
               <Nav className="ml-auto">
                 <Nav.Link
-                  href="https://drive.google.com/file/d/198E1-Tjevc5g3T_Qo6elr_EjCL_ZWTIK/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1DkCfx8PT-lSYyO63e0UPIshMwA4GOpfC/view?usp=sharing"
                   className="hit-me-up"
                 >
                   Resume
